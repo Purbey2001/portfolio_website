cbfd1d5a0d7b6b84560c1aba261295fc66fa1d90
Abhishek Kumar <purbeyabhishek2001@gmail.com>
2025-09-28 20:27:09 +0530

    header added

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <header>
+        <div className='logoHeader'>
+          <div className='logo'>
+            <span>AK</span>
+          </div>
+          <div>
+            <span className='title'>Abhishek Kumar</span>
+            <br />
+            <span>Aspiring Devloper</span>
+          </div>
+        </div>
+        <div className='section'>
+          <button>Home</button>
+          <button>Skills</button>
+          <button>Projects</button>
+          <button>Achievements</button>
+          <button>About</button>
+          <button>Resume</button>
+          <button>Contact</button>
+        </div>
+        <div className='section'>
+          <button>
+            Get In Touch
+          </button>
+        </div>
+      </header>
+      <footer>
+        
+      </footer>
+    </div>
+  )
+}
+
+export default App
