65beda3c006edac0294ef6eacaf503ffcee29adb
Abhishek Kumar <purbeyabhishek2001@gmail.com>
2025-09-29 16:08:17 +0530

    added achievement section

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import './Achievements.css'
+import { FaRegStar } from "react-icons/fa";
+import { BsFillTrophyFill } from "react-icons/bs";
+import { AiOutlineGlobal } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
 
 function Achievements() {
   return (
-    <div>
-      
+    <div className='achievements'>
+      <h1 style={{marginBottom:'0px'}}>Achievements</h1>
+      <p>Milestones and accomplishments in my programming journey</p>
+      <div className='achievement'>
+        <div className='achievement-section'>
+          <div>
+            <FaRegStar />
+          </div>
+          <div className='achievement-card'>
+            <p style={{marginBottom:'4px',marginTop:'4px'}}>Solved 500+ LeetCode Problems</p>
+            <span>Highest Rating 1566</span>
+          </div>
+        </div>
+        <div className='achievement-section'>
+          <div>
+            <BsFillTrophyFill />
+          </div>
+          <div className='achievement-card'>
+            <p style={{marginBottom:'4px',marginTop:'4px'}}>3★ CodeChef</p>
+            <span>Highest Rating 1699</span>
+          </div>
+        </div>
+        <div className='achievement-section'>
+          <div>
+            <AiOutlineGlobal />
+          </div>
+          <div className='achievement-card'>
+            <p style={{marginBottom:'4px',marginTop:'4px'}}>Global Rank 197
+</p>
+            <span>CodeChef Starters 141</span>
+          </div>
+        </div>
+        <div className='achievement-section'>
+          <div>
+            <FaGithub />
+          </div>
+          <div className='achievement-card'>
+            <p style={{marginBottom:'4px',marginTop:'4px'}}>Open Source Contributor
+</p>
+            <span>Multiple GitHub repositories</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import './Skills.css'
 
 function Skills() {
   return (
-    <div>
-      
+    <div className='skills'>
+      <h1 style={{marginBottom:'0px'}}>Skills & Technologies</h1>
+      <p>Here are the technologies and tools I work with to bring ideas to life</p>
+      <div className='skill-card'>
+        <div className='skill-section'>
+          <p><span>Languages : </span> C++, JavaScript, Java</p>
+        </div>
+        <div className='skill-section'>
+          <p><span>Front-end : </span> ReactJs, Redux Toolkit, HTML, CSS,Bootstrap, Tailwind CSS</p>
+        </div>
+        <div className="skill-section">
+          <p><span>Database : </span>PostgreSql, MongoDb, MySql</p>
+        </div>
+        <div className='skill-section'>
+          <p><span>Back-end :</span> NodeJs(basic),ExpressJs(basic) , MongoDb(basic)</p>
+        </div>
+        <div className='skill-section'>
+          <p><span>Tools & Version Control :</span> Git, GitHub, VS Code</p>
+        </div>
+        <div className="skill-section">
+          <p><span>Core CS Concepts: : </span>DSA, DBMS, OOPs</p>
+        </div>
+        <div className="skill-section">
+          <p><span>Soft-Skills : </span>Requirement Gathering, Team Collaboration, Problem-Solving</p>
+        </div>
+      </div>
     </div>
   )
 }
